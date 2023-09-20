c9283fc7db4f6d9a981f5b809d3c7dbc12ec402b
Ted M. Young <tedyoung@gmail.com>
2023-09-20 11:27:30 -0700

    LAB #1: Made Static and Moved display() method to new class

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -1,7 +1,5 @@
 package com.jitterted.ebp.blackjack;
 
-import org.fusesource.jansi.Ansi;
-
 import static org.fusesource.jansi.Ansi.ansi;
 
 public class Card {
@@ -15,24 +13,6 @@ public class Card {
 
     public int rankValue() {
         return rank.value();
-    }
-
-    public String display() {
-        String[] lines = new String[7];
-        lines[0] = "┌─────────┐";
-        lines[1] = String.format("│%s%s       │", rank().display(), rank() == Rank.TEN ? "" : " ");
-        lines[2] = "│         │";
-        lines[3] = String.format("│    %s    │", suit().symbol());
-        lines[4] = "│         │";
-        lines[5] = String.format("│       %s%s│", rank() == Rank.TEN ? "" : " ", rank().display());
-        lines[6] = "└─────────┘";
-
-        Ansi.Color cardColor = suit().isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
-        return ansi()
-                .fg(cardColor).toString()
-                + String.join(ansi().cursorDown(1)
-                                    .cursorLeft(11)
-                                    .toString(), lines);
     }
 
     public Suit suit() {
