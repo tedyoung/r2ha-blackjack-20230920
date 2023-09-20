ca385791b382518748bcb98a81c817f25e6b039d
Ted M. Young <tedyoung@gmail.com>
2023-09-20 12:17:12 -0700

    LAB #2: Extracted query method for faceUpCard(), which returns an immutable Card

--- src/main/java/com/jitterted/ebp/blackjack/Hand.java
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -36,7 +36,11 @@ public class Hand {
     }
 
     String displayFaceUpCard() {
-        return ConsoleCard.display(cards.get(0));
+        return ConsoleCard.display(faceUpCard());
+    }
+
+    public Card faceUpCard() {
+        return cards.get(0);
     }
 
     boolean dealerMustDrawCard() {
