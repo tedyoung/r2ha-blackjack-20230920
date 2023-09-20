49f811765235441da2304cdde36fa59e912cba63
Ted M. Young <tedyoung@gmail.com>
2023-09-20 16:46:29 -0700

    LAB #4: Using a new programmable stub, the StubDeck, we can control the cards and order that they're drawn to make a predictable test.

--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -1,12 +1,16 @@
 package com.jitterted.ebp.blackjack.domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class GameOutcomeTest {
 
-//    @Test
+    @Test
     void playerHitsAndGoesBustThenOutcomeIsPlayerLoses() {
-        Game game = new Game(new ShuffledDeck());
+        Game game = new Game(new StubDeck(Rank.TEN, Rank.EIGHT,
+                                          Rank.QUEEN, Rank.JACK,
+                                          Rank.THREE));
         game.initialDeal();
 
         game.playerHits();
