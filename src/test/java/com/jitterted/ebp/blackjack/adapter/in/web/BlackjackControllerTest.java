51cdbd91f04d59a545af8f0eb45e985666fc8c42
Ted M. Young <tedyoung@gmail.com>
2023-09-27 11:23:27 -0700

    TDD: Get to /game endpoint returns correct view name

--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
@@ -1,8 +1,15 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
+import com.jitterted.ebp.blackjack.domain.Card;
+import com.jitterted.ebp.blackjack.domain.Deck;
 import com.jitterted.ebp.blackjack.domain.Game;
+import com.jitterted.ebp.blackjack.domain.Rank;
 import com.jitterted.ebp.blackjack.domain.ShuffledDeck;
+import com.jitterted.ebp.blackjack.domain.StubDeck;
+import com.jitterted.ebp.blackjack.domain.Suit;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -19,5 +26,20 @@ class BlackjackControllerTest {
                 .hasSize(2);
         assertThat(redirectPage)
                 .isEqualTo("redirect:/game");
+    }
+
+    @Test
+    void gameViewPopulatesViewModelWithAllCards() {
+        Deck stubDeck = new StubDeck(List.of(new Card(Suit.DIAMONDS, Rank.TEN),
+                                             new Card(Suit.HEARTS, Rank.TWO),
+                                             new Card(Suit.DIAMONDS, Rank.KING),
+                                             new Card(Suit.CLUBS, Rank.THREE)));
+        Game game = new Game(stubDeck);
+        BlackjackController blackjackController = new BlackjackController(game);
+        blackjackController.startGame();
+
+        String viewName = blackjackController.gameView();
+        assertThat(viewName)
+                .isEqualTo("blackjack");
     }
 }
--- src/test/java/com/jitterted/ebp/blackjack/domain/StubDeck.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/StubDeck.java
@@ -16,6 +16,10 @@ public class StubDeck implements Deck {
         this.iterator = cards.listIterator();
     }
 
+    public StubDeck(List<Card> cards) {
+        this.iterator = cards.listIterator();
+    }
+
     static Deck playerHitsAndGoesBust() {
         return new StubDeck(Rank.TEN, Rank.EIGHT,
                             Rank.QUEEN, Rank.JACK,
