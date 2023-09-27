61f9ad187119472e6c38ab6e60c60688ac4419d6
Ted M. Young <tedyoung@gmail.com>
2023-09-27 10:59:25 -0700

    TDD: startGame() calls Game#initialDeal() (though Spring isn't happy)

---
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
@@ -0,0 +1,21 @@
+package com.jitterted.ebp.blackjack.adapter.in.web;
+
+import com.jitterted.ebp.blackjack.domain.Game;
+import com.jitterted.ebp.blackjack.domain.ShuffledDeck;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BlackjackControllerTest {
+
+    @Test
+    void startGameResultsInTwoCardsDealtToPlayer() {
+        Game game = new Game(new ShuffledDeck());
+        BlackjackController blackjackController = new BlackjackController(game);
+
+        blackjackController.startGame();
+
+        assertThat(game.playerHand().cards())
+                .hasSize(2);
+    }
+}
