21d63f3c251cbc6c215346cb246642339b6bbd64
Ted M. Young <tedyoung@gmail.com>
2023-09-27 11:03:19 -0700

    TDD: Make Spring happy by providing a bridge (@Bean configuration) to access the Game object

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -1,6 +1,7 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
 import com.jitterted.ebp.blackjack.domain.Game;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -9,6 +10,7 @@ public class BlackjackController {
 
     private final Game game;
 
+    @Autowired
     public BlackjackController(Game game) {
         this.game = game;
     }
