8fe98ea5cdf2871f37db06189854344d09025bed
Ted M. Young <tedyoung@gmail.com>
2023-09-27 15:37:07 -0700

    LAB #8A: Added backwards-compatible ("Parallel Change") Game constructor that takes a GameMonitor reference

--- src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
@@ -1,8 +1,11 @@
 package com.jitterted.ebp.blackjack.domain;
 
+import com.jitterted.ebp.blackjack.domain.port.GameMonitor;
+
 public class Game {
 
     private final Deck deck;
+    private final GameMonitor gameMonitor;
 
     private final Hand dealerHand = new Hand();
     private final Hand playerHand = new Hand();
@@ -10,7 +13,12 @@ public class Game {
     private boolean playerDone;
 
     public Game(Deck deck) {
+        this(deck, game -> {});
+    }
+
+    public Game(Deck deck, GameMonitor gameMonitor) {
         this.deck = deck;
+        this.gameMonitor = gameMonitor;
     }
 
     public void initialDeal() {
---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameMonitorTest.java
@@ -0,0 +1,15 @@
+package com.jitterted.ebp.blackjack.domain;
+
+import com.jitterted.ebp.blackjack.domain.port.GameMonitor;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.spy;
+
+class GameMonitorTest {
+
+    @Test
+    void playerStandsThenGameIsOverAndResultsSentToMonitor() {
+        GameMonitor gameMonitorSpy = spy(GameMonitor.class);
+        Game game = new Game(StubDeck.playerStandsAndBeatsDealer(), gameMonitorSpy);
+    }
+}
